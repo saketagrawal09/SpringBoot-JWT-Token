6539dbac4a3a82ec2ad2f3307cfd5ffe22635719
Saket Agrawal <saketagrawal09@gmail.com>
2020-05-05 23:11:01 +0530

    first commit

---
+++ src/main/java/com/springboot/jwt/SpringBootJWTToken/SpringBootJwtTokenApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.jwt.SpringBootJWTToken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtTokenApplication.class, args);
+	}
+
+}
